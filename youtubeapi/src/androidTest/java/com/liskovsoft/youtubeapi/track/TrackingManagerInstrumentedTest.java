2a90af31c0840b4f203b72e0518a219de94f6b79
Yuriy Liskov <firsthash@gmail.com>
2020-08-18 01:22:02 +0300

    refactor

--- youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/track/TrackingManagerInstrumentedTest.java
+++ youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/track/TrackingManagerInstrumentedTest.java
@@ -1,5 +1,6 @@
 package com.liskovsoft.youtubeapi.track;
 
+import com.liskovsoft.sharedutils.okhttp.OkHttpHelpers;
 import com.liskovsoft.youtubeapi.app.AppService;
 import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper;
 import com.liskovsoft.youtubeapi.common.helpers.TestHelpers;
@@ -12,6 +13,8 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -34,24 +37,24 @@ public class TrackingManagerInstrumentedTest {
         VideoInfoResult videoInfo = mVideoInfoService.getVideoInfo(TestHelpers.VIDEO_ID_SIMPLE);
 
         String videoId = videoInfo.getVideoDetails().getVideoId();
-        float lengthSeconds = Float.parseFloat(videoInfo.getVideoDetails().getLengthSeconds());
-        float currentTimeSeconds = 10;
+        float lengthSec = Float.parseFloat(videoInfo.getVideoDetails().getLengthSeconds());
+        float positionSec = 10;
         String authorization = TestHelpers.getAuthorization();
 
-        Call<WatchTimeResult> wrapper = mTrackingManager.initWatchTime(
-                videoId, lengthSeconds, currentTimeSeconds, currentTimeSeconds, currentTimeSeconds,
+        Call<WatchTimeResult> wrapper = mTrackingManager.updateWatchTime(
+                videoId, lengthSec, positionSec, positionSec, positionSec,
                 playbackNonce, videoInfo.getEventId(), authorization);
 
         Response<WatchTimeResult> response = wrapper.execute();
 
-        assertTrue("Watch time init success", response.isSuccessful());
-
-        wrapper = mTrackingManager.updateWatchTime(
-                videoId, lengthSeconds, currentTimeSeconds, currentTimeSeconds, currentTimeSeconds,
-                playbackNonce, videoInfo.getEventId(), authorization);
-
-        response = wrapper.execute();
-
         assertTrue("Watch time update success", response.isSuccessful());
+
+        //Map<String, String> headers = new HashMap<>();
+        //headers.put("Authorization", "Bearer ya29.a0AfH6SMC3jB2_4za-2d3u32WKM2wDtsJreFoC2f3lvbWWz7IybkkohC3WpvcTvmGfNSXJi2rgBNd96TrykVj5mV4UsGQ163HGPwF-AkvER1NYzS2ze3UxXECU5tYs7ALfvFc9ZyoWNsUt7jXnCtmaTraPzs1Sj1Asf87RUC43oMAvuw");
+        //
+        //OkHttpHelpers.doGetOkHttpRequest(
+        //        "https://www.youtube.com/api/stats/watchtime?ns=yt&ver=2&final=1" +
+        //                "&docid=" + videoId + "&len=" + lengthSec + "&cmt=" + positionSec + "&st=" + positionSec +
+        //                "&et=" + positionSec + "&cpn=" + playbackNonce + "&ei=" + videoInfo.getEventId(), headers);
     }
 }

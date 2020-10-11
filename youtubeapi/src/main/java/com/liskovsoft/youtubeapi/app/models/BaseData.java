efc20d4aa6e3f9ac4aa5155b84dcce610b460bd4
Yuriy Liskov <firsthash@gmail.com>
2020-10-11 07:13:23 +0300

    upd

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/BaseData.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/BaseData.java
@@ -3,12 +3,18 @@ package com.liskovsoft.youtubeapi.app.models;
 import com.liskovsoft.youtubeapi.common.converters.regexp.RegExp;
 
 public class BaseData {
-    @RegExp({"clientId:\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}:\"\\w+\"", // Cobalt
-            "\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}=\"\\w+\""})
+    @RegExp({
+            "\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",\\n[\\w]{2}=\"\\w+\"", // Cobalt/Legacy
+            "\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}=\"\\w+\"", // Modern (always presents)
+            "clientId:\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}:\"\\w+\"" // Cobalt/Legacy (always presents)
+            })
     private String mClientId;
 
-    @RegExp({"clientId:\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}:\"(\\w+)\"", // Cobalt
-            "\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}=\"(\\w+)\""})
+    @RegExp({
+            "\"[-\\w]+\\.apps\\.googleusercontent\\.com\",\\n[\\w]{2}=\"(\\w+)\"", // Cobalt/Legacy
+            "\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}=\"(\\w+)\"", // Modern (always presents)
+            "clientId:\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}:\"(\\w+)\"" // Cobalt/Legacy (always presents)
+            })
     private String mClientSecret;
 
     public String getClientId() {

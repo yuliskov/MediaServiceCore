c9474fdb2ad9f1f652d8fdcd239eef0f08cbe2f7
Yuriy Liskov <firsthash@gmail.com>
2020-10-11 06:48:21 +0300

    upd

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/BaseData.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/BaseData.java
@@ -3,10 +3,12 @@ package com.liskovsoft.youtubeapi.app.models;
 import com.liskovsoft.youtubeapi.common.converters.regexp.RegExp;
 
 public class BaseData {
-    @RegExp("\"([-\\w]+\\.apps\\.googleusercontent\\.com)\"")
+    @RegExp({"clientId:\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}:\"\\w+\"", // Cobalt
+            "\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}=\"\\w+\""})
     private String mClientId;
 
-    @RegExp("\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}=\"(\\w+)\"")
+    @RegExp({"clientId:\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}:\"(\\w+)\"", // Cobalt
+            "\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}=\"(\\w+)\""})
     private String mClientSecret;
 
     public String getClientId() {

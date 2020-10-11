726c1917fb5bc811812384a05396ff38a82a8d32
Yuriy Liskov <firsthash@gmail.com>
2020-10-11 14:02:15 +0300

    refactor

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/LegacyClientData.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/models/LegacyClientData.java
@@ -6,12 +6,10 @@ import com.liskovsoft.youtubeapi.common.converters.regexp.RegExp;
  * Cobalt/Legacy
  */
 public class LegacyClientData implements ClientData {
-    @RegExp({"\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",\\n[\\w]{2}=\"\\w+\"",
-            "clientId:\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}:\"\\w+\""})
+    @RegExp("clientId:\"([-\\w]+\\.apps\\.googleusercontent\\.com)\",[\\w]{2}:\"\\w+\"")
     private String mClientId;
 
-    @RegExp({"\"[-\\w]+\\.apps\\.googleusercontent\\.com\",\\n[\\w]{2}=\"(\\w+)\"",
-            "clientId:\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}:\"(\\w+)\""})
+    @RegExp("clientId:\"[-\\w]+\\.apps\\.googleusercontent\\.com\",[\\w]{2}:\"(\\w+)\"")
     private String mClientSecret;
 
     @Override

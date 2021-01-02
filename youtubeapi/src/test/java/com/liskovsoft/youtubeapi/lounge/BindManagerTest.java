5ba5dcd5bbebac66b689aa3783ebfda83e4869ce
Yuriy Liskov <firsthash@gmail.com>
2021-01-02 18:58:01 +0200

    refactor

--- youtubeapi/src/test/java/com/liskovsoft/youtubeapi/lounge/BindManagerTest.java
+++ youtubeapi/src/test/java/com/liskovsoft/youtubeapi/lounge/BindManagerTest.java
@@ -1,9 +1,31 @@
 package com.liskovsoft.youtubeapi.lounge;
 
+import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowLog;
 
 @RunWith(RobolectricTestRunner.class)
 public class BindManagerTest {
+    private BindManager mBindManager;
+    private ScreenManager mScreenManager;
 
+    @Before
+    public void setUp() {
+        // fix issue: No password supplied for PKCS#12 KeyStore
+        // https://github.com/robolectric/robolectric/issues/5115
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
+        ShadowLog.stream = System.out; // catch Log class output
+
+        mBindManager = RetrofitHelper.withRegExp(BindManager.class);
+        mScreenManager = RetrofitHelper.withJsonPath(ScreenManager.class);
+    }
+
+    @Test
+    public void testThatPairingCodeGeneratedSuccessfully() {
+        
+    }
 }

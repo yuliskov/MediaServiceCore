fb4fbacc1e03082b7a0170a6c9a4d4ddf15e0f85
Yuriy Liskov <firsthash@gmail.com>
2021-01-09 07:13:16 +0200

    refactor

--- youtubeapi/src/test/java/com/liskovsoft/youtubeapi/lounge/BindManagerTest.java
+++ youtubeapi/src/test/java/com/liskovsoft/youtubeapi/lounge/BindManagerTest.java
@@ -1,18 +1,32 @@
 package com.liskovsoft.youtubeapi.lounge;
 
+import com.itkacher.okhttpprofiler.OkHttpProfilerInterceptor;
+import com.liskovsoft.sharedutils.okhttp.OkHttpHelpers;
 import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper;
 import com.liskovsoft.youtubeapi.lounge.models.Command;
 import com.liskovsoft.youtubeapi.lounge.models.CommandInfos;
-import com.liskovsoft.youtubeapi.lounge.models.ScreenInfos;
 import com.liskovsoft.youtubeapi.lounge.models.PairingCode;
 import com.liskovsoft.youtubeapi.lounge.models.Screen;
 import com.liskovsoft.youtubeapi.lounge.models.ScreenId;
+import com.liskovsoft.youtubeapi.lounge.models.ScreenInfos;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Request.Builder;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowLog;
 import retrofit2.Call;
+
+import javax.annotation.Nullable;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -21,6 +35,13 @@ public class BindManagerTest {
     private static final String SCREEN_NAME = "TubeNext";
     private static final String LOUNGE_TOKEN_TMP = "AGdO5p8cH1tKYW3OIVFhSMRfjAjV5OxqYdjCezBGrDAaX7be3bcttKQAVKucSpEcoi8qh6rYs_r04DXQhd0_xEZY69s8W5J7rqEMmeaYwJsSi5VivgnFKv4";
     private static final String SCREEN_ID_TMP = "910nbko7d2d6qtthu2609a3id6";
+    private static final String BIND2_URL = "https://www.youtube.com/api/lounge/bc/bind?" +
+            "device=LOUNGE_SCREEN&theme=cl&capabilities=dsp%2Cmic%2Cdpa&mdxVersion=2&VER=8&v=2&t=1" +
+            "&RID=rpc&CI=0" +
+            "&app=" + BindManagerParams.APP +
+            "&id=" + BindManagerParams.SCREEN_UID +
+            "&AID=" + BindManagerParams.AID +
+            "&zx=" + BindManagerParams.ZX;
     private BindManager mBindManager;
     private ScreenManager mScreenManager;
     private CommandManager mCommandManager;
@@ -54,6 +75,85 @@ public class BindManagerTest {
         CommandInfos bindData = getFirstBind();
 
         assertNotNull("Contains bind data", bindData);
+    }
+
+    @Test
+    public void testWebSocket() throws InterruptedException {
+        CommandInfos firstBind = getFirstBind();
+        Command command1 = firstBind.getCommands().get(0);
+        Command command2 = firstBind.getCommands().get(1);
+        String screenId = command1.getCommandParams().get(0);
+        String sessionId = command2.getCommandParams().get(0);
+
+        Request request = new Builder().url(String.format(
+                "%s&name=%s&loungeIdToken=%s&SID=%s&gsessionid=%s",
+                BIND2_URL, SCREEN_NAME, LOUNGE_TOKEN_TMP, screenId, sessionId
+        )).build();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        //builder.addInterceptor(new OkHttpProfilerInterceptor());
+
+        builder.addInterceptor(new Interceptor() {
+            @Override public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request()
+                        .newBuilder()
+                        .removeHeader("Upgrade")
+                        .removeHeader("Connection")
+                        .removeHeader("Sec-WebSocket-Key")
+                        .removeHeader("Sec-WebSocket-Version")
+                        .build();
+                return chain.proceed(request);
+            }
+        });
+
+        OkHttpClient okHttpClient = builder.build();
+
+        //okHttpClient.networkInterceptors().add(new Interceptor() {
+        //    @Override public Response intercept(Chain chain) throws IOException {
+        //        Request request = chain.request()
+        //                .newBuilder()
+        //                .removeHeader("Upgrade")
+        //                .removeHeader("Connection")
+        //                .removeHeader("Sec-WebSocket-Key")
+        //                .removeHeader("Sec-WebSocket-Version")
+        //                .build();
+        //        return chain.proceed(request);
+        //    }
+        //});
+
+        okHttpClient.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, Response response) {
+                super.onOpen(webSocket, response);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, String text) {
+                super.onMessage(webSocket, text);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, ByteString bytes) {
+                super.onMessage(webSocket, bytes);
+            }
+
+            @Override
+            public void onClosing(WebSocket webSocket, int code, String reason) {
+                super.onClosing(webSocket, code, reason);
+            }
+
+            @Override
+            public void onClosed(WebSocket webSocket, int code, String reason) {
+                super.onClosed(webSocket, code, reason);
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, @Nullable Response response) {
+                super.onFailure(webSocket, t, response);
+            }
+        });
+
+        Thread.sleep(100_000);
     }
 
     @Test

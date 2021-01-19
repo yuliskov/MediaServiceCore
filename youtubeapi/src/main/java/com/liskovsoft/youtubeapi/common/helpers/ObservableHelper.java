9724d5be81488d3ccfffe3196228b71bb0b73d0a
Yuriy Liskov <firsthash@gmail.com>
2021-01-20 00:47:58 +0200

    rxjava: error handling fix

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/common/helpers/ObservableHelper.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/common/helpers/ObservableHelper.java
@@ -1,12 +1,16 @@
 package com.liskovsoft.youtubeapi.common.helpers;
 
+import com.liskovsoft.sharedutils.mylogger.Log;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.exceptions.OnErrorNotImplementedException;
 
 import java.util.concurrent.Callable;
 
 public class ObservableHelper {
+    private static final String TAG = ObservableHelper.class.getSimpleName();
+
     public static Observable<Void> fromVoidable(Runnable callback) {
         return Observable.create(emitter -> {
             callback.run();
@@ -40,8 +44,12 @@ public class ObservableHelper {
     }
 
     public static <T> void onError(ObservableEmitter<T> emitter, String msg) {
-        // Fix fall back on the global error handler
-        // More info: https://stackoverflow.com/questions/44420422/crash-when-sending-exception-through-rxjava
-        emitter.tryOnError(new IllegalStateException(msg));
+        try {
+            // Fix fall back on the global error handler
+            // More info: https://stackoverflow.com/questions/44420422/crash-when-sending-exception-through-rxjava
+            emitter.tryOnError(new IllegalStateException(msg));
+        } catch (OnErrorNotImplementedException e) {
+            Log.e(TAG, "Oops. I forgot to add error handler somewhere. But it's ok. %s", e.getMessage());
+        }
     }
 }

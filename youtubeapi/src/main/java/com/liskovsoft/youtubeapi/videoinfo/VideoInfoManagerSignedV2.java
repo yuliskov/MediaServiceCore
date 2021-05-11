35d113bc036c48399913dfca9c699922c0c6cb0d
Yuriy Liskov <firsthash@gmail.com>
2021-05-11 15:34:47 +0300

    refactor

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/VideoInfoManagerSignedV2.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/VideoInfoManagerSignedV2.java
@@ -4,7 +4,6 @@ import com.liskovsoft.youtubeapi.app.AppConstants;
 import com.liskovsoft.youtubeapi.videoinfo.models.VideoInfo;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 /**
@@ -13,22 +12,22 @@ import retrofit2.http.Query;
  */
 public interface VideoInfoManagerSignedV2 {
     // Unused method. sts - ???
-    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv")
+    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv&cver=" + AppConstants.CLIENT_VERSION)
     Call<VideoInfo> getVideoInfo(@Query("video_id") String videoId, @Query("sts") String sts, @Query("access_token") String auth);
     
-    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv")
+    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv&cver=" + AppConstants.CLIENT_VERSION)
     Call<VideoInfo> getVideoInfo(@Query("video_id") String videoId, @Query("access_token") String auth);
 
     /**
      * Unlock live hls streams
      */
-    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv")
+    @GET("https://www.youtube.com/get_video_info?ps=leanback&el=leanback&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv&cver=" + AppConstants.CLIENT_VERSION)
     Call<VideoInfo> getVideoInfoHls(@Query("video_id") String videoId, @Query("hl") String lang, @Query("access_token") String auth);
 
     /**
      * Unlock age restricted videos
      */
-    @GET("https://www.youtube.com/get_video_info?ps=default&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv")
+    @GET("https://www.youtube.com/get_video_info?ps=default&eurl=https%3A%2F%2Fwww.youtube.com%2Ftv&cver=" + AppConstants.CLIENT_VERSION)
     Call<VideoInfo> getVideoInfoRestricted(@Query("video_id") String videoId, @Query("access_token") String auth);
 
     /**

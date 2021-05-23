dd07aa1979954831e4141ac02cde53f876304af5
Yuriy Liskov <firsthash@gmail.com>
2021-05-23 19:19:12 +0300

    test upd

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/V2/VideoInfoManagerParams.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/V2/VideoInfoManagerParams.java
@@ -3,15 +3,10 @@ package com.liskovsoft.youtubeapi.videoinfo.V2;
 import com.liskovsoft.youtubeapi.common.helpers.ServiceHelper;
 
 public class VideoInfoManagerParams {
-    private static final String VIDEO_ID = "\"videoId\":\"%s\",\"ps\":\"%s\"";
+    private static final String VIDEO_ID = "\"videoId\":\"%s\",\"racyCheckOk\":\"true\""; // Confirm age
 
     public static String getVideoInfoQuery(String videoId) {
-        String channelTemplate = String.format(VIDEO_ID, videoId, "leanback");
-        return ServiceHelper.createQuery(channelTemplate);
-    }
-
-    public static String getRestrictedVideoInfoQuery(String videoId) {
-        String channelTemplate = String.format(VIDEO_ID, videoId, "default");
+        String channelTemplate = String.format(VIDEO_ID, videoId);
         return ServiceHelper.createQuery(channelTemplate);
     }
 }
---
+++ youtubeapi/src/test/java/com/liskovsoft/youtubeapi/videoinfo/VideoInfoManagerSignedV2Test.java
@@ -0,0 +1,122 @@
+package com.liskovsoft.youtubeapi.videoinfo;
+
+import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper;
+import com.liskovsoft.youtubeapi.common.helpers.tests.TestHelpersV1;
+import com.liskovsoft.youtubeapi.common.helpers.tests.TestHelpersV2;
+import com.liskovsoft.youtubeapi.common.locale.LocaleManager;
+import com.liskovsoft.youtubeapi.videoinfo.V2.VideoInfoManagerParams;
+import com.liskovsoft.youtubeapi.videoinfo.V2.VideoInfoManagerSigned;
+import com.liskovsoft.youtubeapi.videoinfo.V2.VideoInfoManagerUnsigned;
+import com.liskovsoft.youtubeapi.videoinfo.models.CaptionTrack;
+import com.liskovsoft.youtubeapi.videoinfo.models.VideoInfo;
+import com.liskovsoft.youtubeapi.videoinfo.models.formats.AdaptiveVideoFormat;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowLog;
+import retrofit2.Call;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class VideoInfoManagerSignedV2Test {
+    private VideoInfoManagerSigned mService;
+    private LocaleManager mLocaleManager;
+
+    @Before
+    public void setUp() {
+        // fix issue: No password supplied for PKCS#12 KeyStore
+        // https://github.com/robolectric/robolectric/issues/5115
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
+        ShadowLog.stream = System.out; // catch Log class output
+
+        RetrofitHelper.sForceEnableProfiler = true;
+
+        mService = RetrofitHelper.withJsonPath(VideoInfoManagerSigned.class);
+        mLocaleManager = LocaleManager.instance();
+    }
+
+    @Test
+    public void testThatAgeRestrictedVideoContainsRequiredFields() throws IOException {
+        testThatNonLiveVideoInfoContainsRequiredFields(getVideoInfoRestricted(TestHelpersV1.VIDEO_ID_AGE_RESTRICTED));
+    }
+
+    @Test
+    public void testThatUnavailableVideoContainsRequiredFields() throws IOException {
+        testThatNonLiveVideoInfoContainsRequiredFields(getVideoInfo(TestHelpersV1.VIDEO_ID_UNAVAILABLE));
+    }
+
+    @Test
+    public void testThatLiveVideoContainsSpecificFields()  throws IOException {
+        testThatLiveVideoContainsSpecificFields(getVideoInfo(TestHelpersV1.VIDEO_ID_LIVE));
+    }
+
+    @Test
+    public void testThatVideoWithCaptionsContainsRequiredFields() throws IOException {
+        testThatVideoWithCaptionsContainsRequiredFields(getVideoInfo(TestHelpersV1.VIDEO_ID_CAPTIONS));
+    }
+
+    private void testThatLiveVideoContainsSpecificFields(VideoInfo result) {
+        assertNotNull("Result not null", result);
+        assertNotNull("Contains dash url", result.getDashManifestUrl());
+        // V2 doesn't contains legacy hls urls
+        //assertNotNull("Contains hls url", result.getHlsManifestUrl());
+
+        testThatVideoInfoContainsRequiredFields(result);
+    }
+
+    private void testThatVideoWithCaptionsContainsRequiredFields(VideoInfo result) {
+        assertNotNull("Result not null", result);
+        List<CaptionTrack> captionTracks = result.getCaptionTracks();
+        assertNotNull("Contains captions", captionTracks);
+        CaptionTrack track = captionTracks.get(0);
+
+        assertNotNull("Contains name", track.getName());
+        assertNotNull("Contains base url", track.getBaseUrl());
+        assertNotNull("Contains vss id", track.getVssId());
+        assertNotNull("Contains lang", track.getLanguageCode());
+        assertNotNull("Contains mime type", track.getMimeType());
+        assertNotNull("Contains codecs", track.getCodecs());
+
+        assertTrue("Subtitle url exists", TestHelpersV1.urlExists(track.getBaseUrl()));
+
+        testThatNonLiveVideoInfoContainsRequiredFields(result);
+    }
+
+    private void testThatNonLiveVideoInfoContainsRequiredFields(VideoInfo result) {
+        List<AdaptiveVideoFormat> formats = result.getAdaptiveFormats();
+        assertNotNull("Contains range", formats.get(0).getIndexRange());
+
+        testThatVideoInfoContainsRequiredFields(result);
+    }
+
+    private void testThatVideoInfoContainsRequiredFields(VideoInfo result) {
+        assertNotNull("Result not null", result);
+        assertFalse("Video available externally", result.isUnplayable());
+        List<AdaptiveVideoFormat> formats = result.getAdaptiveFormats();
+        assertTrue("Formats not empty", formats.size() > 0);
+        assertTrue("Contains fps", formats.get(0).getFps() != 0);
+        assertTrue("Contains bitrate", formats.get(0).getBitrate() != 0);
+        assertNotNull("Contains tracking url", result.getVideoStatsWatchTimeUrl());
+        assertNotNull("Contains video details", result.getVideoDetails());
+        assertNotNull("Contains event id", result.getEventId());
+        assertNotNull("Contains vm tracking param", result.getVisitorMonitoringData());
+    }
+
+    private VideoInfo getVideoInfoRestricted(String videoId) throws IOException {
+        Call<VideoInfo> wrapper = mService.getVideoInfo(VideoInfoManagerParams.getVideoInfoQuery(videoId), TestHelpersV2.getAuthorization());
+        return wrapper.execute().body();
+    }
+
+    private VideoInfo getVideoInfo(String videoId) throws IOException {
+        Call<VideoInfo> wrapper = mService.getVideoInfo(VideoInfoManagerParams.getVideoInfoQuery(videoId), TestHelpersV2.getAuthorization());
+        return wrapper.execute().body();
+    }
+}

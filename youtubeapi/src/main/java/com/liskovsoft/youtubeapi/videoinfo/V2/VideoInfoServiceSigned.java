63f5f09f60cec70920c9d426f10177d07a055914
Yuriy Liskov <firsthash@gmail.com>
2021-05-23 22:22:19 +0300

    upd

---
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/V2/VideoInfoServiceSigned.java
@@ -0,0 +1,44 @@
+package com.liskovsoft.youtubeapi.videoinfo.V2;
+
+import com.liskovsoft.sharedutils.mylogger.Log;
+import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper;
+import com.liskovsoft.youtubeapi.videoinfo.VideoInfoServiceBase;
+import com.liskovsoft.youtubeapi.videoinfo.models.VideoInfo;
+import retrofit2.Call;
+
+public class VideoInfoServiceSigned extends VideoInfoServiceBase {
+    private static final String TAG = VideoInfoServiceSigned.class.getSimpleName();
+    private static VideoInfoServiceSigned sInstance;
+    private final VideoInfoManagerSigned mVideoInfoManagerSigned;
+
+    private VideoInfoServiceSigned() {
+        mVideoInfoManagerSigned = RetrofitHelper.withJsonPath(VideoInfoManagerSigned.class);
+    }
+
+    public static VideoInfoServiceSigned instance() {
+        if (sInstance == null) {
+            sInstance = new VideoInfoServiceSigned();
+        }
+
+        return sInstance;
+    }
+
+    public VideoInfo getVideoInfo(String videoId, String authorization) {
+        VideoInfo result = getVideoInfoRegular(videoId, authorization);
+
+        if (result != null) {
+            decipherFormats(result.getAdaptiveFormats());
+            decipherFormats(result.getRegularFormats());
+        } else {
+            Log.e(TAG, "Can't get video info. videoId: %s, authorization: %s", videoId, authorization);
+        }
+
+        return result;
+    }
+
+    private VideoInfo getVideoInfoRegular(String videoId, String authorization) {
+        Call<VideoInfo> wrapper = mVideoInfoManagerSigned.getVideoInfo(VideoInfoManagerParams.getVideoInfoQuery(videoId), authorization);
+
+        return RetrofitHelper.get(wrapper);
+    }
+}

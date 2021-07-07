582cbf39723787a692c356039d0fc2e30c3ab091
Yuriy Liskov <firsthash@gmail.com>
2021-07-07 13:44:02 +0300

    refactor

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/browse/models/sections/SectionContinuation.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/browse/models/sections/SectionContinuation.java
@@ -12,11 +12,21 @@ public class SectionContinuation {
     @JsonPath("$.continuationContents.horizontalListContinuation.continuations[0].nextContinuationData.continuation")
     private String mNextPageKey;
 
+    /**
+     * Generic wrapper if there's no continuation content
+     */
+    @JsonPath("$.continuationContents.genericPromoContinuation.bodyText.runs[0].text")
+    private String mPromoContinuationText;
+
     public String getNextPageKey() {
         return mNextPageKey;
     }
 
     public List<ItemWrapper> getItemWrappers() {
         return mItemWrappers;
+    }
+
+    public String getPromoContinuationText() {
+        return mPromoContinuationText;
     }
 }

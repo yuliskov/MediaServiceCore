119029b5ce54d45e0e052390c823709da4c3d863
Yuriy Liskov <firsthash@gmail.com>
2021-09-11 04:46:24 +0300

    kotlin support tests

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v2/result/WatchNextResult.kt
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/next/v2/result/WatchNextResult.kt
@@ -1,3 +1,23 @@
 package com.liskovsoft.youtubeapi.next.v2.result
 
-data class WatchNextResult(val data: String)
+import com.liskovsoft.youtubeapi.common.helpers.ServiceHelper
+
+// "$.contents.singleColumnWatchNextResults.pivot.pivot.contents[*].shelfRenderer"
+// "$.title.runs[0].text", "$.title.simpleText"
+
+data class WatchNextResult(val contents: Contents?) {
+    data class Contents(val singleColumnWatchNextResults: SingleColumnWatchNextResults?) {
+        data class SingleColumnWatchNextResults(val pivot: Pivot?) {
+            data class Pivot(val pivot: Pivot?, val contents: List<Content>?) {
+                data class Content(val shelfRenderer: ShelfRenderer?) {
+                    data class ShelfRenderer(val title: Title?) {
+                        data class Title(val runs: List<TextItem>?, val simpleText: String?) {
+                            data class TextItem(val text: String?)
+                            fun getText() = if (runs != null) ServiceHelper.combineText(runs) else simpleText
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ youtubeapi/src/test/java/com/liskovsoft/youtubeapi/next/v2/WatchNextManagerUnsignedTest.kt
@@ -0,0 +1,74 @@
+package com.liskovsoft.youtubeapi.next.v2
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.junit.Before
+import org.robolectric.shadows.ShadowLog
+import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper
+import com.liskovsoft.youtubeapi.common.locale.LocaleManager
+import com.liskovsoft.youtubeapi.browse.BrowseManagerParams
+import com.liskovsoft.youtubeapi.next.WatchNextManagerParams
+import com.liskovsoft.youtubeapi.common.helpers.tests.TestHelpersV1
+import com.liskovsoft.youtubeapi.next.result.WatchNextResult
+import org.junit.Assert
+import org.junit.Test
+
+@RunWith(RobolectricTestRunner::class)
+class WatchNextManagerUnsignedTest {
+    private var mManager: WatchNextManagerUnsigned? = null
+    @Before
+    fun setUp() {
+        // fix issue: No password supplied for PKCS#12 KeyStore
+        // https://github.com/robolectric/robolectric/issues/5115
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS")
+        ShadowLog.stream = System.out // catch Log class output
+        mManager = RetrofitHelper.withGson(WatchNextManagerUnsigned::class.java)
+    }
+
+    @Test
+    fun testSuggestedItemsNotNull() {
+        val watchNextQuery = WatchNextManagerParams.getWatchNextQuery(TestHelpersV1.VIDEO_ID_CAPTIONS)
+        val wrapper = mManager!!.getWatchNextResult(watchNextQuery)
+        val watchNextResult = RetrofitHelper.get(wrapper)
+
+        Assert.assertNotNull("watchNextResult not null", watchNextResult)
+    }
+
+//    @Test
+//    fun testThatWatchNextContainsAllRequiredFields() {
+//        checkWatchNextResultFields(watchNextResult)
+//    }
+//
+//    @Test
+//    fun testThatResultProperlyLocalized() {
+//        LocaleManager.instance().language = "en"
+//        var watchNextResult = watchNextResult
+//        var firstSuggesting = watchNextResult.suggestedSections[0]
+//        Assert.assertEquals("Suggestion title is localized to english", "Suggestions", firstSuggesting.title)
+//        LocaleManager.instance().language = "ru"
+//        watchNextResult = watchNextResult
+//        firstSuggesting = watchNextResult.suggestedSections[0]
+//        Assert.assertEquals("Suggestion title is localized to russian", "Рекомендации", firstSuggesting.title)
+//    }
+//
+//    @Test
+//    fun testThatWatchNextRowsCouldBeContinued() {
+//        val watchNextResult = watchNextResult
+//        val rootNextPageKey = watchNextResult.suggestedSections[0].nextPageKey
+//        Assert.assertNotNull("Root contains next key", rootNextPageKey)
+//        val continuation = continueWatchNext(rootNextPageKey)
+//        val nextPageKey = continuation.nextPageKey
+//        Assert.assertNotNull("Continuations contains next key", nextPageKey)
+//    }
+//
+//    private fun continueWatchNext(nextPageKey: String): WatchNextResultContinuation {
+//        val wrapper = mManager!!.continueWatchNextResult(BrowseManagerParams.getContinuationQuery(nextPageKey))
+//        return RetrofitHelper.get(wrapper)
+//    }
+//
+//    private val watchNextResult: WatchNextResult
+//        private get() {
+//            val wrapper = mManager!!.getWatchNextResult(WatchNextManagerParams.getWatchNextQuery(TestHelpersV1.VIDEO_ID_CAPTIONS))
+//            return RetrofitHelper.get(wrapper)
+//        }
+}

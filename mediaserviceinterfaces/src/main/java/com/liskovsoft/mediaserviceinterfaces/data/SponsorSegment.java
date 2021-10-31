797ed57c512aef10f7bcc6654a764dd73a8f93d2
Yuriy Liskov <firsthash@gmail.com>
2021-10-31 20:38:42 +0200

    more sb categories

--- mediaserviceinterfaces/src/main/java/com/liskovsoft/mediaserviceinterfaces/data/SponsorSegment.java
+++ mediaserviceinterfaces/src/main/java/com/liskovsoft/mediaserviceinterfaces/data/SponsorSegment.java
@@ -7,6 +7,8 @@ public interface SponsorSegment {
     String CATEGORY_INTERACTION = "interaction";
     String CATEGORY_SELF_PROMO = "selfpromo";
     String CATEGORY_MUSIC_OFF_TOPIC = "music_offtopic";
+    String CATEGORY_PREVIEW_RECAP = "preview";
+    String CATEGORY_HIGHLIGHT = "poi_highlight";
 
     long getStartMs();
     long getEndMs();

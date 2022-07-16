10fdf1a7afc9abfda5e39ab14dcef4bb8f6fb9e7
Yuriy Liskov <firsthash@gmail.com>
2022-07-16 20:38:40 +0300

    chat: remove custom emoji (can't be displayed)

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/common/models/kt/ItemHelper.kt
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/common/models/kt/ItemHelper.kt
@@ -5,9 +5,13 @@ import com.liskovsoft.youtubeapi.service.YouTubeMediaServiceHelper
 fun TextItem.getText() = runs?.joinToString("") { it?.text ?: it?.emoji?.getText() ?: "" } ?: simpleText
 
 /**
- * Custom emoji should be rendered as images. Images not supported at this moment. Use shortcut name as workaround.
+ * Use shortcut name as workaround to display custom emoji. Custom emoji are images.
  */
-fun LiveChatEmoji.getText() = if (isCustomEmoji == true) shortcuts?.getOrElse(0) { "" } else emojiId
+//fun LiveChatEmoji.getText() = if (isCustomEmoji == true) shortcuts?.getOrElse(0) { "" } else emojiId
+/**
+ * Use empty string as workaround to display custom emoji. Custom emoji are images.
+ */
+fun LiveChatEmoji.getText() = if (isCustomEmoji == true) "" else emojiId
 
 /**
  * Find optimal thumbnail for tv screen

6a82a39575d2d0f0ac9bef2bd8a560e5eb6a173c
Yuriy Liskov <firsthash@gmail.com>
2024-10-16 17:30:53 +0300

    token tests refactor

--- youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/potoken/PoToken.kt
+++ youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/potoken/PoToken.kt
@@ -1,65 +1,9 @@
 package com.liskovsoft.youtubeapi.potoken
 
 import com.google.gson.JsonElement
+import com.liskovsoft.sharedutils.TestHelpers
 import com.liskovsoft.youtubeapi.common.helpers.RetrofitHelper
 import com.liskovsoft.youtubeapi.common.js.V8Runtime
-
-private const val RESULT_DELIM = "%RESULT_DELIM%"
-private const val DOM_WRAPPER =
-    """
-        const base64urlCharRegex = /[-_.]/g;
-        const base64urlToBase64Map = {
-            '-': '+',
-            _: '/',
-            '.': '='
-        };
-        function base64ToU8(base64) {
-            let base64Mod;
-            if (base64urlCharRegex.test(base64)) {
-                base64Mod = base64.replace(base64urlCharRegex, function (match) {
-                    return base64urlToBase64Map[match];
-                });
-            }
-            else {
-                base64Mod = base64;
-            }
-            base64Mod = atob(base64Mod);
-            const result = new Uint8Array([...base64Mod].map((char) => char.charCodeAt(0)));
-            return result;
-        }
-        function u8ToBase64(u8, base64url = false) {
-            const result = btoa(String.fromCharCode(...u8));
-            if (base64url) {
-                return result
-                    .replace(/\+/g, '-')
-                    .replace(/\//g, '_');
-            }
-            return result;
-        }
-        function btoa(input) {
-          const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-          let str = String(input);
-          let output = '';
-          let i = 0;
-        
-          while (i < str.length) {
-            const block = (str.charCodeAt(i++) << 16) |
-                          (str.charCodeAt(i++) << 8) |
-                          (str.charCodeAt(i++) || 0);
-        
-            output += chars[(block >> 18) & 0x3F] +
-                      chars[(block >> 12) & 0x3F] +
-                      (i > str.length + 1 ? '=' : chars[(block >> 6) & 0x3F]) +
-                      (i > str.length ? '=' : chars[block & 0x3F]);
-          }
-        
-          return output;
-        }
-        window = {}; window.document = {}; document = {};
-        window.performance = { now: () => 261855.69999999553 };
-        window.btoa = btoa;
-        document.hidden = false;
-    """
 
 internal class PoToken {
     data class Arguments(val privateScript: String?,
@@ -76,10 +20,14 @@ internal class PoToken {
 
     private data class BotGuardResult(val integrityTokenData: Data?, val postProcessFunction: String?)
 
+    /**
+     * Generates a Proof of Origin Token.
+     * @param args - The arguments for generating the token.
+     */
     fun generate(args: Arguments): Result? {
         val bgResult = invokeBotGuard(args.privateScript, args.program, args.globalName, args.bgConfig) ?: return null
 
-        if (bgResult.postProcessFunction?.isEmpty() == true) throw IllegalStateException("Couldn't get postProcessFunction")
+        if (bgResult.postProcessFunction?.isEmpty() == true) throw IllegalStateException("postProcessFunction cannot be empty")
 
         val script = listOf(
             DOM_WRAPPER.trimIndent(),
@@ -96,13 +44,25 @@ internal class PoToken {
         return Result(poToken, bgResult.integrityTokenData)
     }
 
-    fun generatePlaceholder(visitorData: String): String? {
+    /**
+     * Creates a placeholder PoToken. This can be used while `sps` (StreamProtectionStatus) is 2, but will not work once it changes to 3.
+     * @param identifier - Visitor ID or Data Sync ID.
+     */
+    fun generatePlaceholder(identifier: String, clientState: Int? = null): String? {
         return null
     }
 
+    /**
+     * Invokes the Botguard VM.
+     * @param program - The bytecode to run.
+     * @param globalName - The name of the VM in the global scope.
+     * @param bgConfig - The Botguard configuration.
+     */
     private fun invokeBotGuard(privateScript: String?, program: String?, globalName: String?, bgConfig: BotGuardConfig): BotGuardResult? {
         val script = listOf(
             DOM_WRAPPER.trimIndent(),
+            //TestHelpers.readResource("potoken/bundle.js"),
+            //"var mydom = new MyLibrary.JSDOM(); window = mydom.window; document = mydom.window.document;",
             privateScript,
             """
                var vm = $globalName;
@@ -122,7 +82,7 @@ internal class PoToken {
                var first = '';
                if (postProcessFunctions.length > 0) 
                     first = postProcessFunctions[0].name;
-               first + "$RESULT_DELIM" + second;
+               first + "$RESULT_DELIM" + second;   
             """.trimIndent()
         )
 
---
+++ youtubeapi/src/androidTest/java/com/liskovsoft/youtubeapi/potoken/const.kt
@@ -0,0 +1,77 @@
+package com.liskovsoft.youtubeapi.potoken
+
+internal const val RESULT_DELIM = "%RESULT_DELIM%"
+internal const val DOM_WRAPPER =
+"""
+    const base64urlCharRegex = /[-_.]/g;
+    const base64urlToBase64Map = {
+        '-': '+',
+        _: '/',
+        '.': '='
+    };
+    function base64ToU8(base64) {
+        let base64Mod;
+        if (base64urlCharRegex.test(base64)) {
+            base64Mod = base64.replace(base64urlCharRegex, function (match) {
+                return base64urlToBase64Map[match];
+            });
+        }
+        else {
+            base64Mod = base64;
+        }
+        base64Mod = atob(base64Mod);
+        const result = new Uint8Array([...base64Mod].map((char) => char.charCodeAt(0)));
+        return result;
+    }
+    function u8ToBase64(u8, base64url = false) {
+        const result = btoa(String.fromCharCode(...u8));
+        if (base64url) {
+            return result
+                .replace(/\+/g, '-')
+                .replace(/\//g, '_');
+        }
+        return result;
+    }
+    function btoa(input) {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        let str = String(input);
+        let output = '';
+        let i = 0;
+        
+        while (i < str.length) {
+            const block = (str.charCodeAt(i++) << 16) |
+                          (str.charCodeAt(i++) << 8) |
+                          (str.charCodeAt(i++) || 0);
+            
+            output += chars[(block >> 18) & 0x3F] +
+                      chars[(block >> 12) & 0x3F] +
+                      (i > str.length + 1 ? '=' : chars[(block >> 6) & 0x3F]) +
+                      (i > str.length ? '=' : chars[block & 0x3F]);
+        }
+        
+        return output;
+    }
+    var events = {};
+    function addEventListener(event, handler) {
+        if (!events[event]) {
+          events[event] = [];
+        }
+        events[event].push(handler);
+    }
+    function removeEventListener(event, handler) {
+        if (!events[event]) return;
+        events[event] = events[event].filter(h => h !== handler);
+    }
+    function dispatchEvent(event) {
+        if (!events[event.type]) return;
+        events[event.type].forEach(handler => handler(event));
+    }
+    window = {}; document = {}; window.document = document;
+    window.performance = { now: () => 261855.69999999553 };
+    window.btoa = btoa;
+    document.hidden = false;
+    document.readyState = 'complete';
+    document.addEventListener = addEventListener;
+    document.removeEventListener = removeEventListener;
+    document.dispatchEvent = dispatchEvent;
+"""

49c644bcad32f5b18e1f981d15d62186905dac64
Yuriy Liskov <firsthash@gmail.com>
2024-12-13 06:23:23 +0200

    const upd

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokencloud/Constants.kt
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokencloud/Constants.kt
@@ -1,3 +1,6 @@
 package com.liskovsoft.youtubeapi.app.potokencloud
 
-internal const val PO_TOKEN_CLOUD_BASE_URL = "https://smarttubeservice.heroku.com"
+internal val PO_TOKEN_CLOUD_BASE_URLS = arrayOf(
+    "https://service1.com",
+    "https://service2.onrender.com"
+)

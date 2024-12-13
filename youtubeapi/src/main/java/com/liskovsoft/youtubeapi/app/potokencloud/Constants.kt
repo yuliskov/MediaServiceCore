d7c0c366d7de62bcea2eaf1385bff80c74bef302
Yuriy Liskov <firsthash@gmail.com>
2024-12-13 06:23:46 +0200

    const upd

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokencloud/Constants.kt
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokencloud/Constants.kt
@@ -2,5 +2,5 @@ package com.liskovsoft.youtubeapi.app.potokencloud
 
 internal val PO_TOKEN_CLOUD_BASE_URLS = arrayOf(
     "https://service1.com",
-    "https://service2.onrender.com"
+    "https://service2.com"
 )

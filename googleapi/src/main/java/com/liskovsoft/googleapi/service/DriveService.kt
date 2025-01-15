34045b43cff44b2fbffcad06450150d83aa2eda3
Yuriy Liskov <firsthash@gmail.com>
2025-01-15 19:53:28 +0200

    drive service refactor

---
+++ googleapi/src/main/java/com/liskovsoft/googleapi/service/DriveService.kt
@@ -0,0 +1,30 @@
+package com.liskovsoft.googleapi.service
+
+import android.net.Uri
+import com.liskovsoft.googleapi.drive3.DriveServiceInt
+import com.liskovsoft.sharedutils.rx.RxHelper
+import io.reactivex.Observable
+import java.io.File
+import java.io.InputStream
+
+object DriveService {
+    @JvmStatic
+    fun uploadFile(file: File, path: Uri): Observable<Void> {
+        return RxHelper.fromRunnable { DriveServiceInt.uploadFile(file, path) }
+    }
+
+    @JvmStatic
+    fun uploadFile(content: String, path: Uri): Observable<Void> {
+        return RxHelper.fromRunnable { DriveServiceInt.uploadFile(content, path) }
+    }
+
+    @JvmStatic
+    fun getFile(path: Uri): Observable<InputStream?> {
+        return RxHelper.fromCallable { DriveServiceInt.getFile(path) }
+    }
+
+    @JvmStatic
+    fun getList(path: Uri): Observable<List<String?>?> {
+        return RxHelper.fromCallable { DriveServiceInt.getList(path) }
+    }
+}

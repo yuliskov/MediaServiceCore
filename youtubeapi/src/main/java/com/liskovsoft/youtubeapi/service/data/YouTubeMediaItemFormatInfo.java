2f70730743e1ccaaef8c1ffdecc2e0c392217b90
Yuriy Liskov <firsthash@gmail.com>
2025-05-21 12:35:55 +0300

    auto volume fix

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/service/data/YouTubeMediaItemFormatInfo.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/service/data/YouTubeMediaItemFormatInfo.java
@@ -291,12 +291,20 @@ public class YouTubeMediaItemFormatInfo implements MediaItemFormatInfo {
         //    result *= multiplier; // minimize distortions
         //}
 
+        //if (mLoudnessDb != 0) {
+        //    // Original tv web: Math.min(1, 10 ** (-loudnessDb / 20))
+        //    // -5db...5db (0.7...1.4) Base formula: normalLevel*10^(-db/20)
+        //    float normalLevel = (float) Math.pow(10.0f, -mLoudnessDb / 15.0f);
+        //    result = Math.min(normalLevel, 4f);
+        //    result *= 0.3f; // minimize distortions
+        //}
+
         if (mLoudnessDb != 0) {
             // Original tv web: Math.min(1, 10 ** (-loudnessDb / 20))
             // -5db...5db (0.7...1.4) Base formula: normalLevel*10^(-db/20)
-            float normalLevel = (float) Math.pow(10.0f, -mLoudnessDb / 15.0f);
-            result = Math.min(normalLevel, 4f);
-            result *= 0.3f; // minimize distortions
+            float normalLevel = (float) Math.pow(10.0f, mLoudnessDb / 20.0f);
+            // The max volume should be no more than 1.5 to minimize distortions
+            result = Math.min(normalLevel, 1.5f);
         }
 
         return result;

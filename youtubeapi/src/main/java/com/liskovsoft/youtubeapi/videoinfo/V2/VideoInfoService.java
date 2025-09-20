a19efefb05db5e6d35076f6702f91a7c3f59837e
Yuriy Liskov <firsthash@gmail.com>
2025-09-20 03:00:39 +0300

    video info: depreoritize Android client

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/V2/VideoInfoService.java
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/videoinfo/V2/VideoInfoService.java
@@ -35,8 +35,8 @@ public class VideoInfoService extends VideoInfoServiceBase {
     // VIDEO_INFO_TV and VIDEO_INFO_TV_EMBED are the only ones working in North America
     // VIDEO_INFO_MWEB - can bypass SABR-only responses
     private final static AppClient[] VIDEO_INFO_TYPE_LIST = {
-            AppClient.ANDROID_REEL, // doesn't require pot
             AppClient.WEB_EMBED,
+            AppClient.ANDROID_REEL, // doesn't require pot
             AppClient.IOS,
             AppClient.TV,
             AppClient.TV_EMBED, // single audio language

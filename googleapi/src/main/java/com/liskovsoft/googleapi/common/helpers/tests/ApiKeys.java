c71a5ffb9542be8491cfdf645a8b853a3e5ca337
Yuriy Liskov <firsthash@gmail.com>
2026-03-26 02:59:48 +0200

    cleanup

--- googleapi/src/main/java/com/liskovsoft/googleapi/common/helpers/tests/ApiKeys.java
+++
@@ -1,7 +0,0 @@
-package com.liskovsoft.googleapi.common.helpers.tests;
-
-public class ApiKeys {
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
-    public static final String REFRESH_TOKEN = "";
-}

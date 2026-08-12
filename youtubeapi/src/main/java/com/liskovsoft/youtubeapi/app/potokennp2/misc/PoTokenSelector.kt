3d9521ffc3c9f450e2c0cb7807a89a8eca156bf7
Yuriy Liskov <firsthash@gmail.com>
2026-08-12 23:38:59 +0300

    PoTokenSelector: use sabr compatible generator

--- youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokennp2/misc/PoTokenSelector.kt
+++ youtubeapi/src/main/java/com/liskovsoft/youtubeapi/app/potokennp2/misc/PoTokenSelector.kt
@@ -1,6 +1,6 @@
 package com.liskovsoft.youtubeapi.app.potokennp2.misc
 
 import com.liskovsoft.youtubeapi.app.potokennp2.core.PoTokenGenerator
-import com.liskovsoft.youtubeapi.app.potokennp2.generators.PoTokenWebView
+import com.liskovsoft.youtubeapi.app.potokennp2.generators.PoTokenWebView4
 
-internal fun selectFactory(): PoTokenGenerator.Factory = PoTokenWebView
+internal fun selectFactory(): PoTokenGenerator.Factory = PoTokenWebView4
